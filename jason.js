8016a39cb80f697604b2d340e4f02731c187bc22
mapplevn <viniciuslimanascimento1201@gmail.com>
2022-11-28 14:30:20 -0300

    site atualizado

---
+++ jason.js
@@ -0,0 +1,23 @@
+'user strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('fileImg');
+
+photo.addEventListener('click', () => {
+    file.click();
+});
+
+file.addEventListener('change', (event) => {
+
+    if(file.files.length <=0) {
+        return;
+    }
+
+    let reader = new FileReader();
+    
+    reader.onload = () => {
+        photo.src = reader.result;
+    }
+
+    reader.readAsDataURL(file.files[0]);
+});
